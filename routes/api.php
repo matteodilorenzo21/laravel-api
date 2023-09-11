306245d6fad9e27e6928fbde07dcbf7336f827a8
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-09-11 17:37:59 +0200

    added route for project list & project api controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Project List Route
+Route::get('/projects', function () {
+    $projects = Project::all();
+    return response()->json($projects);
 });
